8055626f9f765f80472e990083f95ff63d5c893c
Mamadou Saliou Diallo <mamadousaliou.diallo.963434@gmail.com>
2024-11-21 15:50:49 +0000

    managing projects links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,18 +5,22 @@ const Projects = () => {
     {
       title: "TJM Simulator",
       description: "A resposive TJM simulator for freelance with the possibility to use african currency.",
+      href: "https://github.com/Masaliou224/TJM-NAWAARI"
     },
     {
       title: "MarketPlace",
       description: "An online marketplace for sellers and buyers.",
+      href: "https://github.com/Masaliou224/loumo-place.git"
     },
     {
       title: "N'mafôkhai",
       description: "N'mafôkhai is a website for connecting homeowners and tenants.",
+      href: "https://github.com/Masaliou224/N-maf-khai.git"
     },
     {
       title: "Audio Player",
       description: "Audio-Player app developed in React.JS and Next.JS.",
+      href: "https://github.com/Masaliou224/Audio-Player.git"
     },
   ];
 
@@ -29,8 +33,10 @@ const Projects = () => {
             key={index}
             className="p-6 border rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 dark:bg-gray-300"
           >
+            <a href={project.href} >
             <h3 className="text-xl font-bold text-gray-800">{project.title}</h3>
             <p className="mt-2 text-gray-600">{project.description}</p>
+            </a>
           </div>
         ))}
       </div>
